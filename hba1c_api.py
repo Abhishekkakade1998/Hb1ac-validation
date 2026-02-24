257e3adc7565b69c465bda6c70acc2faf20c24b7
Abhi_sk <abhishekkakade803@gmail.com>
2026-02-24 13:28:48 +0530

    Update hba1c_api.py

--- hba1c_api.py
+++ hba1c_api.py
@@ -1,9 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+# -------------------------------
+
+# -------------------------------
+@app.route("/")
+def home():
+    return render_template("index.html"))
 
 # ==========================================================
 # Utility: Expected HbA1c from Glucose
